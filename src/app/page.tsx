26ddcc9e7f62c4f602cae1f561955b62f8cfc875
Francis <32603796+fsilva7456@users.noreply.github.com>
2025-01-06 22:37:11 -0500

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { CompetitiveAnalysis } from '@/components/competitive-analysis'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-8">
+      <CompetitiveAnalysis />
+    </main>
+  )
+}
